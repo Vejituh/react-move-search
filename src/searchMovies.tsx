8035a258a55391b63e3b4966ccb1ba19d19cba54
Rafael <rafaelg1402@protonmail.com>
2021-01-18 13:54:51 +0000

    add apiUrl and static Query

--- src/searchMovies.tsx
+++ src/searchMovies.tsx
@@ -2,8 +2,25 @@ import React from "react";
 import "./searchMovies.css";
 
 export default function SearchMovies() {
+  const searchMovies = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log("submiting");
+
+    const query = "Star Wars";
+
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=301fea267f554470429f50bff9e67513&language=en-GB&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
