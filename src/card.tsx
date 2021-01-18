806f5e2200ad87305c72316a9eb965ed8f8d8a97
Rafael <rafaelg1402@protonmail.com>
2021-01-18 18:26:50 +0000

    add card component and styling

---
+++ src/card.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./card.css";
+
+export default function Card(props: any) {
+  const getStarRating = () => {
+    let starAmount = (props.vote_average / 2 / 5) * 100;
+    return starAmount;
+  };
+  return (
+    <div className="card">
+      {props.poster_path !== null ? (
+        <img
+          className="card-poster"
+          alt="movie poser"
+          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.poster_path}`}
+        />
+      ) : null}
+      <h1>{props.original_title}</h1>
+      <div className="stars-overlay">
+        <div
+          className="stars-innerlay"
+          style={{ width: `${getStarRating()}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
