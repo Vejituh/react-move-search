d8d3c232062b6cd82bd2473c7a42f2fb6b35e68c
Rafael <rafaelg1402@protonmail.com>
2021-01-18 18:26:19 +0000

    add state for movies and search

--- src/searchMovies.tsx
+++ src/searchMovies.tsx
@@ -1,38 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./searchMovies.css";
+import Card from "./card";
 
 export default function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    console.log("submiting");
-
-    const query = "Star Wars";
 
     const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=301fea267f554470429f50bff9e67513&language=en-GB&query=${query}&page=1&include_adult=false`;
 
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
   };
 
+  function handleChange(e: React.FormEvent<HTMLInputElement>) {
+    const value = e.currentTarget.value;
+    setQuery(value);
+  }
+
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        type="text"
-        name="query"
-        placeholder="i.e Star Wars"
-        className="input"
-      ></input>
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          name="query"
+          placeholder="i.e Star Wars"
+          className="input"
+          value={query}
+          onChange={handleChange}
+        ></input>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies.map((movie: any) => (
+          <Card key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   );
 }
